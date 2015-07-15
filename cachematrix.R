02a7c87e6b8441b30693e22344b313d82102603f
datacourse2015 <datacourse2015@gmail.comgit config --global user.email datacourse2015@gmail.com>
2015-07-15 12:33:51 +0100

    edit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+        ##Peer Assessments /Programming Assignment 2: Lexical Scoping 
+        ## **Please Note: No Grace Period**
+        
+        ## The following functions makeCacheMatrix & cacheSolve, together 
+        ## create a square invertible matrix, and then return the 
+        ## inverse of the matrix available from the cache environment
 
-## Write a short comment describing this function
 
+        ## makeCacheMatrix creates an invertible matrix,
+        ## returning a list containing functions to:
+        ##              1. set the value of the matrix
+        ##              2. get the value of the matrix
+        ##              3. set the value of the inverse
+        ##              4. get the value of the inverse
+        ## this list is used as the input to cacheSolve()
+        
 makeCacheMatrix <- function(x = matrix()) {
-
+    cache <- NULL
+    set <- function(y) {
+        x <<- y
+        cache <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) cache <<- inverse
+    getinverse <- function() cache
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
+        ## cacheSolve computes the inverse of the “matrix” returned by 
+        ## makeCacheMatrix(). If the inverse is already calculated with
+        ## no change to the matrix, cacheSolve retrieves the inverse 
+        ## from the cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {        
         ## Return a matrix that is the inverse of 'x'
+    cache <- x$getinverse()
+    if(!is.null(cache)) {
+        message("getting cached data")
+        return(cache)
+    }
+    data <- x$get()
+    cache <- solve(data, ...)
+    x$setinverse(cache)
+    cache
+}
 }
